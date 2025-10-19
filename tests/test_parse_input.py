cf41f0b99beabce24fb2b0c9e5dc8b143827ec67
Michał <michal.pryslopski@gmail.com>
2025-10-19 10:16:07 +0200

    improved tests

--- tests/test_parse_input.py
+++ tests/test_parse_input.py
@@ -1,10 +1,3 @@
-'''
-2025-07-07 09:00:00 Pola Chabrów 2025 | Zenon Kuśmider (Korona) : Krashan Bhamaradżanga (Kozacy) | 3 (13) : 0 (3)
-2025-07-07 09:00:00 Pola Chabrów 2025 | Hans Gonschorek (Tatarzy) : Stanisław Krokodyl (Korona) | 1 (7) : 1 (7)
-2025-07-07 13:00:00 Pola Chabrów 2025 | Krashan Bhamaradżanga (Kozacy) : Hans Gonschorek (Tatarzy) | 0 (7) : 3 (8)
-2025-07-07 13:00:00 Pola Chabrów 2025 | Zenon Kuśmider (Korona) : Stanisław Krokodyl (Korona) | 3 (14) : 0 (4)
-2025-08-01 09:00:00 Bitka o Jabłko 2025 | Hans Gonschorek (Tatarzy) : Stanisław Krokodyl (Korona) | 3 (18) : 0 (0)
-'''
 import unittest
 from src.score import Score
 import src.parse_input as parse_input
@@ -13,9 +6,9 @@ class Test_ParseInput(unittest.TestCase):
 
     def test_LoadScores(self):
         scores = parse_input.load_scores('tests/test_scores.csv')
-        self.assertEqual(scores[0].__str__(),'2025-07-07 09:00:00 Pola Chabrów 2025 | Zenon Kuśmider (Korona) : Krashan Bhamaradżanga (Kozacy) | 3 (13) : 0 (3)')
-        self.assertEqual(scores[1].__str__(),'2025-07-07 09:00:00 Pola Chabrów 2025 | Hans Gonschorek (Tatarzy) : Stanisław Krokodyl (Korona) | 1 (7) : 1 (7)')
-        self.assertEqual(scores[2].__str__(),'2025-07-07 13:00:00 Pola Chabrów 2025 | Krashan Bhamaradżanga (Kozacy) : Hans Gonschorek (Tatarzy) | 0 (7) : 3 (8)')
-        self.assertEqual(scores[3].__str__(),'2025-07-07 13:00:00 Pola Chabrów 2025 | Zenon Kuśmider (Korona) : Stanisław Krokodyl (Korona) | 3 (14) : 0 (4)')
-        self.assertEqual(scores[4].__str__(),'2025-08-01 09:00:00 Bitka o Jabłko 2025 | Hans Gonschorek (Tatarzy) : Stanisław Krokodyl (Korona) | 3 (18) : 0 (0)')
+        self.assertEqual(scores[1].__str__(),'2025-07-07 09:00:00 Pola Chabrów 2025 | Zenon Kuśmider (Korona) : Krashan Bhamaradżanga (Kozacy) | 3 (13) : 0 (3)')
+        self.assertEqual(scores[2].__str__(),'2025-07-07 09:00:00 Pola Chabrów 2025 | Hans Gonschorek (Tatarzy) : Stanisław Krokodyl (Korona) | 1 (7) : 1 (7)')
+        self.assertEqual(scores[3].__str__(),'2025-07-07 13:00:00 Pola Chabrów 2025 | Krashan Bhamaradżanga (Kozacy) : Hans Gonschorek (Tatarzy) | 0 (7) : 3 (8)')
+        self.assertEqual(scores[4].__str__(),'2025-07-07 13:00:00 Pola Chabrów 2025 | Zenon Kuśmider (Korona) : Stanisław Krokodyl (Korona) | 3 (14) : 0 (4)')
+        self.assertEqual(scores[0].__str__(),'2025-08-01 09:00:00 Bitka o Jabłko 2025 | Hans Gonschorek (Tatarzy) : Stanisław Krokodyl (Korona) | 3 (18) : 0 (0)')
 
---
+++ tests/test_score.py
@@ -0,0 +1,13 @@
+import unittest
+from src.score import Score
+
+class Test_Score(unittest.TestCase):
+
+    def test_SetUpScore(self):
+
+        s = Score()
+        self.assertEqual(s.__str__(),'1974-07-07 12:00:00 T | P1 (A1) : P2 (A2) | 0 (0) : 0 (0)')
+        s.setArmy1('a1').setArmy2('a2').setPlayer1('p1').setPlayer2('p2')
+        s.setDatetime('2009-10-08 11:00').setTournament('t')
+        s.setVictoryPoints1(4).setVictoryPoints2(2).setTournamentPoints1(3).setTournamentPoints2(1)
+        self.assertEqual(s.__str__(),'2009-10-08 11:00:00 t | p1 (a1) : p2 (a2) | 3 (4) : 1 (2)')
