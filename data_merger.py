ba8ab81ed9238dce0deb21a3d4fb2ec09bdc2282
Michał <michal.pryslopski@gmail.com>
2025-10-28 14:07:01 +0100

    Allowing to merge many raw files

--- data_merger.py
+++ data_merger.py
@@ -10,8 +10,43 @@ from pathlib import Path
 
 logging.basicConfig(level=logging.INFO, format='\t%(message)s')
 
-results_file = 'data/Facts/2023_06_18_Warszawa.csv'
-players_file = 'data/Raw/2023_06_18_Warszawa_Players.csv'
+files_to_process = [
+    '2023_06_18_Warszawa',
+    '2023_07_22_M1_Krakow',
+    '2023_09_23_PolaChwaly',
+    '2023_11_18_Poznan',
+    '2024_01_28_Poznan',
+    '2024_02_10_M2_Krakow',
+    '2024_05_11_M3_Krakow',
+    '2024_06_15_BoW1',
+    '2024_06_15_Pyra1',
+    '2024_07_28_Pyra2',
+    '2024_08_17_Maraton_Warszawa',
+    '2024_09_07_Kapitularz',
+    '2024_09_28_PolaChwaly',
+    '2024_10_19_Sucha',
+    '2024_11_09_BoW2',
+    '2025_02_01_Wroclaw',
+    '2025_02_22_Pulawy_P',
+    '2025_02_22_Pulawy_Z',
+    '2025_03_22_Wroclaw',
+    '2025_03_29_M4_Krakow',
+    '2025_04_27_Bydgoszcz',
+    '2025_05_10_Lublin',
+    '2025_05_24_Poznan',
+    '2025_06_14_BoW3',
+    '2025_06_29_Bydgoszcz',
+    '2025_07_26_Wroclaw',
+    '2025_08_09_Lublin',
+    '2025_08_17_Poznan',
+    '2025_08_24_Wroclaw',
+    '2025_08_30_Lodz',
+    '2025_09_27_Gdansk',
+    '2025_09_27_PolaChwaly',
+    '2025_10_11_OiM_MHP',
+    '2025_10_18_Sucha'
+] # data/Raw/Scores/
+
 player_master_data_file = "data/MasterData/player.csv"
 
 def load_player_facts(players_file: Path) -> List[Dict]:
@@ -48,28 +83,33 @@ def load_player_facts(players_file: Path) -> List[Dict]:
 
 print("Combining data for Elo OiM from several files!")
 
-scores = parse_input.load_scores(results_file)
-raw_player_facts = load_player_facts(players_file)
-player_facts = {}
-for rpf in raw_player_facts:
-    player_facts[rpf['Player']]={'City': rpf['City'], 'Army': rpf['Army']}
-
 playerMDM = mdm.MasterDataDict()
 parse_input.load_master_data(playerMDM, player_master_data_file)
 
-for s in scores:
-    s.harmonizePlayers(playerMDM)
-    if s.Player1 in player_facts:
-        s.setArmy1(player_facts[s.Player1]['Army'])
-    if s.Player2 in player_facts:
-        s.setArmy2(player_facts[s.Player2]['Army'])
+for f in files_to_process:
+    print(f)
 
-with open('out.csv', 'w', newline='') as csvfile:
-    fieldnames = ['Datetime','Tournament','TournamentRank','Player1','VictoryPoints1',
-                  'TournamentPoints1','TournamentPoints2','VictoryPoints2','Player2',
-                  'Army1','Army2','ScoringType']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+    scores = parse_input.load_scores('data/Raw/Scores/' + f + '.csv')
+    raw_player_facts = load_player_facts('data/Raw/Players/' + f + '_Players.csv')
+    player_facts = {}
+    for rpf in raw_player_facts:
+        player_facts[rpf['Player']]={'City': rpf['City'], 'Army': rpf['Army']}
 
-    writer.writeheader()
     for s in scores:
-        writer.writerow(s.getAsDict(playerNaturalKey=True))
+        s.harmonizePlayers(playerMDM)
+        if s.Player1 in player_facts:
+            s.setArmy1(player_facts[s.Player1]['Army'])
+        if s.Player2 in player_facts:
+            s.setArmy2(player_facts[s.Player2]['Army'])
+
+    out_file = 'data/Facts/' + f + '.csv'
+    with open(out_file, 'w', newline='') as csvfile:
+        logging.info('Writing ' + out_file + ' ...')
+        fieldnames = ['Datetime','Tournament','TournamentRank','Player1','VictoryPoints1',
+                    'TournamentPoints1','TournamentPoints2','VictoryPoints2','Player2',
+                    'Army1','Army2','ScoringType']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+
+        writer.writeheader()
+        for s in scores:
+            writer.writerow(s.getAsDict(playerNaturalKey=True))
